36591b7e7ca9178b5394c3576eefdb2e386f9a51
Jason Johnson <jason.johnson@integrisit.com>
2023-08-22 17:31:45 -0500

    added logging better

--- avimark-service-monitor.ps1
+++ avimark-service-monitor.ps1
@@ -22,13 +22,29 @@
 $servicePattern = "*avimark*"
 $waitInterval = 300  # seconds (5 minutes)
 $startupDelay = 300  # additional seconds to wait after system startup (5 minutes)
-$outputFile = "C:\avimark-service-monitor.txt"
+$outputFile = "C:\avimark-service-monitor"
+
+# Buffer for messages
+$bufferedOutput = @()
+
+function AddToBuffer {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Message
+    )
+
+    $bufferedOutput += $Message
+}
+
+function WriteBufferToFile {
+    $bufferedOutput | Out-File -Path $outputFile -Force
+}
 
 # Check if the system has been up for less than 5 minutes (300 seconds)
 $uptime = (Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime
 $elapsedTime = New-TimeSpan -Start $uptime -End (Get-Date)
 if ($elapsedTime.TotalSeconds -lt 300) {
-    Write-Output "System recently started. Waiting for $startupDelay seconds to allow delayed services to start..." | Out-File $outputFile -Append
+    AddToBuffer "System recently started. Waiting for $startupDelay seconds to allow delayed services to start..."
     Start-Sleep -Seconds $startupDelay
 }
 
@@ -36,38 +52,38 @@ if ($elapsedTime.TotalSeconds -lt 300) {
 $services = Get-Service | Where-Object { $_.DisplayName -like $servicePattern }
 
 if (-not $services) {
-    Write-Output "No services found with display name pattern: $servicePattern"
+    AddToBuffer "No services found with display name pattern: $servicePattern"
     exit
 }
 
 $servicesRunning = $false
 
 foreach ($service in $services) {
-    Write-Output "Checking service: $($service.DisplayName)"
+    AddToBuffer "Checking service: $($service.DisplayName)"
 
     while ($true) {  # Endless loop
         if ($service.Status -eq 'Stopped') {
-            Write-Output "Service $($service.DisplayName) is stopped. Waiting for $waitInterval seconds..."
+            AddToBuffer "Service $($service.DisplayName) is stopped. Waiting for $waitInterval seconds..."
 
             Start-Sleep -Seconds $waitInterval
             $service.Refresh()
 
             if ($service.Status -eq 'Stopped') {
-                Write-Output "Attempting to start the service $($service.DisplayName)..."
+                AddToBuffer "Attempting to start the service $($service.DisplayName)..."
 
                 try {
                     Start-Service -InputObject $service
-                    Write-Output "Service $($service.DisplayName) started successfully."
+                    AddToBuffer "Service $($service.DisplayName) started successfully."
                     Start-Sleep -Seconds $waitInterval
                     $service.Refresh()
 
                     if ($service.Status -eq 'Running') {
                         $servicesRunning = $true
-                        Write-Output "Service $($service.DisplayName) is running. Exiting loop."
+                        AddToBuffer "Service $($service.DisplayName) is running. Exiting loop."
                         break
                     }
                 } catch {
-                    Write-Output "Error starting service $($service.DisplayName): $_"
+                    AddToBuffer "Error starting service $($service.DisplayName): $_"
 
                     # Log the error to the EventLog
                     $evtHash = @{
@@ -83,7 +99,7 @@ foreach ($service in $services) {
             }
         } else {
             $servicesRunning = $true
-            Write-Output "Service $($service.DisplayName) is already running. Exiting loop."
+            AddToBuffer "Service $($service.DisplayName) is already running. Exiting loop."
             break
         }
     }
@@ -91,5 +107,5 @@ foreach ($service in $services) {
 
 # If any services were found running, write output to file
 if ($servicesRunning) {
-    Write-Output "Service $($service.DisplayName) is running." | Out-File $outputFile -Force
+    WriteBufferToFile
 }
